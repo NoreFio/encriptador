6cbce1765474c0e26179ac21043d46b5b592e34a
NoreFio <norelisfiore1978@gmail.com>
2024-08-24 21:02:17 +0200

    Version 02

--- script.js
+++ script.js
@@ -2,31 +2,50 @@ const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
 
 
-
-
-
-
-
-
-
-function btnEncriptar(){
-    const textoEncriptado = encriptar(textArea.value)
-    mensaje.value = textoEncriptado
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
     textArea.value = "";
-    mensaje.style.backgroundImage = "none"
+    mensaje.style.backgroundImage = "none";
 }
 
 
-function encriptar(stringEncriptada){
-    let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase()
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
 
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringEncriptada.includes(matrizCodigo[i][0])){
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
 
         }
 
     }
     return stringEncriptada
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function desencriptar(stringdesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringdesencriptada = stringdesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringdesencriptada.includes(matrizCodigo[i][0])) {
+            stringdesencriptada = stringdesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+
+        }
+
+    }
+    return stringdesencriptada
+}
+function copiar() {
+    const textoCopiado = document.getElementById("mensaje");
+    textoCopiado.select(); // Seleccionar el texto
+    document.execCommand("copy"); // Copiar el texto seleccionado
 }
